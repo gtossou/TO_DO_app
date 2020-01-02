e9aa1640bcbe601fa77b9785a94586253daebe22
gtossou <gil.tososu@gmail.com>
2020-01-02 19:55:46 +0100

    task deletion done

--- js/todo.js
+++ js/todo.js
@@ -38,20 +38,22 @@ createNewTaskDiv();
 
 
 const handleTaskCreation = function(event){
-
     if (event.key === "Enter"){
         newTaskDiv = document.querySelector(".newTaskDiv");
-        console.log(newTaskDiv);
+        console.log(counter);
         taskValue = document.querySelector("#taskInputId1").value;
         scheduleValue = document.querySelector("#tscheduleInputId1").value;
 
         if (taskValue != ""){
-            localStorage.setItem("taskName",{"taskValue" : taskValue,"scheduleValue" : scheduleValue});
+            counter = counter+1;
+            const taskObject = {"taskValue" : taskValue,
+                                "scheduleValue" : scheduleValue}
+
+            localStorage.setItem(`task${counter}`,JSON.stringify(taskObject));
+            console.log(localStorage.getItem(`task${counter}`));
         }
-        console.log(taskValue);
     
-        function create(taskV,scheV) {
-            counter =+ 1;
+        function showTask(taskV,scheV) {
             const savedTaskDiv = document.createElement("div");
             savedTaskDiv.classList.add("savedTaskDiv");
             savedTaskDiv.id = `savedTaskDiv${(counter)}`;
@@ -68,26 +70,85 @@ const handleTaskCreation = function(event){
             }
 
             savedTaskDiv.appendChild(savedTaskParagraph);
-            dropImg = document.createElement("img");
+            dropImg = document.createElement("input");
+            dropImg.type="image";
             dropImg.classList.add("dropImg"); 
+            dropImg.dataset.key = `task${counter}`;
             dropImg.src = "./img/drop.jpeg";
             savedTaskParagraph.appendChild(dropImg);
             console.log(counter);
            
         }
-        return create (taskValue,scheduleValue);
+        return showTask(taskValue,scheduleValue);
         
     };
        
     //showTaskSaved(taskValue,scheduleValue);
 }
 
-const addEnterEventListener = function(){
-    enterInputs = document.querySelectorAll(".enterInput");
-    enterInputs.forEach(function(element) {
-        element.addEventListener("keypress",handleTaskCreation)
+const newTaskEventListener = function(){
+    createInputs = document.querySelectorAll(".enterInput");
+    createInputs.forEach(function(element) {
+        element.addEventListener("keypress",handleTaskCreation);
     });
 }
 
-addEnterEventListener();
+newTaskEventListener();
 
+const handleTaskDrop = function(event){
+    
+    if (event.target.className === "dropImg"){
+        const target = event.target
+        taskKey = target.dataset.key;
+        // console.log(JSON.parse(localStorage.getItem(taskKey)));
+        localStorage.removeItem(taskKey);
+        target.parentElement.parentElement.remove();
+    }
+        // newTaskDiv = document.querySelector(".newTaskDiv");
+        // console.log(counter);
+        // taskValue = document.querySelector("#taskInputId1").value;
+        // scheduleValue = document.querySelector("#tscheduleInputId1").value;
+
+    //     if (taskValue != ""){
+    //         counter = counter+1;
+    //         localStorage.setItem(`task${counter}`,JSON.stringify({"taskValue" : taskValue,"scheduleValue" : scheduleValue}));
+    //         console.log(localStorage.getItem(`task${counter}`));
+    //     }
+    
+    //     function showTask(taskV,scheV) {
+    //         const savedTaskDiv = document.createElement("div");
+    //         savedTaskDiv.classList.add("savedTaskDiv");
+    //         savedTaskDiv.id = `savedTaskDiv${(counter)}`;
+    //         newTaskDiv.insertAdjacentElement("beforebegin",savedTaskDiv) 
+    //         const savedTaskParagraph = document.createElement("p");
+    //         savedTaskParagraph.id = `savedTaskValue${(counter)}`;
+    //         savedTaskParagraph.classList.add("savedTaskValue");
+        
+    //         if (scheV != ""){
+    //             savedTaskParagraph.textContent = `${taskV} At ${scheV}`;
+    //         }
+    //         else{
+    //             savedTaskParagraph.textContent = taskV;
+    //         }
+
+    //         savedTaskDiv.appendChild(savedTaskParagraph);
+    //         dropImg = document.createElement("input");
+    //         dropImg.type="image";
+    //         dropImg.classList.add("dropImg"); 
+    //         dropImg.dataset.key = `task${counter}`;
+    //         dropImg.src = "./img/drop.jpeg";
+    //         savedTaskParagraph.appendChild(dropImg);
+    //         console.log(counter);
+           
+    //     }
+    //     return showTask(taskValue,scheduleValue);
+        
+    // };
+       
+    //showTaskSaved(taskValue,scheduleValue);
+}
+
+const dropTaskEventListener = function(){
+    document.body.addEventListener("click",handleTaskDrop);
+}
+dropTaskEventListener();
